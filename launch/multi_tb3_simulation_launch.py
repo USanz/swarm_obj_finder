9721b75776db7264177b57990da29e3f53420140
Unai Sanz <u.sanz.2019@alumnos.urjc.es>
2023-04-28 16:32:48 +0200

    RViz configuration files modified to visualize the debug markers and images.

--- launch/multi_tb3_simulation_launch.py
+++ launch/multi_tb3_simulation_launch.py
@@ -95,7 +95,7 @@ def generate_launch_description():
 
     declare_rviz_config_file_cmd = DeclareLaunchArgument(
         'rviz_config',
-        default_value=os.path.join(bringup_dir, 'config', 'rviz', 'nav2_namespaced_view.rviz'),
+        default_value=os.path.join(bringup_dir, 'config', 'rviz', 'my_nav2_namespaced_view.rviz'),
         description='Full path to the RVIZ config file to use.')
 
     declare_use_robot_state_pub_cmd = DeclareLaunchArgument(
